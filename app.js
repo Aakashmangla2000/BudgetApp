438ede40d06002dc983dfea4ae80ba792703c39e
Aakash Mangla <akkibaby90@yahoo.com>
2020-05-05 22:12:09 +0530

    Added new item to BudgetController

--- app.js
+++ app.js
@@ -2,7 +2,57 @@
 //BUDGET CONTROLLER 
 var budgetController = (function(){
 
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+ 
+    var data = {
+
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+
+    };
+
+        return {
+            addItem: function(type, des, val){
+
+                if(data.allItems[type].length > 0)
+                iD = data.allItems[type][data.allItems[type].length - 1].id + 1;
+                else
+                iD = 0; 
+
+                if( type === 'exp'){
+                    newItem =  new Expense(iD, des, val);
+                }
+                else{
+                    newItem = new Income(iD, des, val);
+                }
+
+                data.allItems[type].push(newItem);
+                 return newItem;
+            },
+
+            display: function(){
+                console.log(data);
+            }
+        };
+          
 })();
 
 //UI CONTROLLER
@@ -12,7 +62,7 @@ var UIController = (function(){
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        addButton: '. add__btn'
+        addButton: '.add__btn'
     };
 
     return{
@@ -36,22 +86,32 @@ var UIController = (function(){
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
-var DOM = UICtrl.getDomStrings();
-console.log(DOM.addButton);
+    var setupEventListeners = function(){
+        document.querySelector(DOM.addButton).addEventListener('click',ctrlAddItem);
 
-var ctrlAddItem = function(){
-    var input = UICtrl.getInput();
-    console.log(input.description);
-}
+        document.addEventListener('keypress',function(event){
+            if(event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+    };
 
-document.querySelector(DOM.addButton).addEventListener('click',ctrlAddItem);
+    var DOM = UICtrl.getDomStrings();
+    console.log(DOM.addButton);
 
-document.addEventListener('keypress',function(event){
-    if(event.keyCode === 13 || event.which === 13){
-        ctrlAddItem();
+    var ctrlAddItem = function(){
+        var input = UICtrl.getInput();
+        var newItem = budgetController.addItem(input.type,input.description,input.value);
+    };
+
+
+    return {
+        init: function(){
+            console.log('hi');
+            setupEventListeners();
+        }
     }
-});
-
-
 
 })(budgetController, UIController);
+
+controller.init();
